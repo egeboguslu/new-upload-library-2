2739686d677e963f2d4513ecc64d31fec1bf4815
egeboguslu <112990645+egeboguslu@users.noreply.github.com>
2022-11-05 23:58:51 +0100

    Add files via upload

---
+++ 02/projectSE.js
@@ -0,0 +1,32 @@
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) 
+    return
+  
+    let currentDropdown
+    if (isDropdownButton) {
+      currentDropdown = e.target.closest("[data-dropdown]")
+      currentDropdown.classList.toggle("active")
+    }
+  
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      if (dropdown === currentDropdown) return
+      dropdown.classList.remove("active")
+    })
+  })
+
+
+/* MODAL --> https://www.youtube.com/watch?v=TAB_v6yBXIE */
+
+
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+}); 
